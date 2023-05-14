f285c10f0941184d5666d3ca71813be52db7e140
Amakoye <charlesamakoye@gmail.com>
2023-05-14 20:00:55 +0300

    update crud operations to use token

--- frontend/src/app/services/products/products.service.ts
+++ frontend/src/app/services/products/products.service.ts
@@ -3,18 +3,31 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from 'src/types';
 
-const httpOptions = {
+/* const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
   }),
-};
+}; */
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
   private api = 'http://localhost:3000/api/products';
-  constructor(private http: HttpClient) {}
+  httpOptions: HttpHeaders;
+
+  constructor(private http: HttpClient) {
+    this.httpOptions = this.setHeaders();
+  }
+
+  private setHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders()
+      .set('Authorization', `Bearer ${token}`)
+      .set('Content-Type', 'application/json')
+      .set('Header-Name', 'Header-Value');
+  }
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.api);
@@ -25,14 +38,20 @@ export class ProductsService {
   }
 
   addProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.api, product, httpOptions);
+    return this.http.post<Product>(this.api, product, {
+      headers: this.httpOptions,
+    });
   }
 
   editProduct(id: string, product: Product): Observable<Product> {
-    return this.http.put<Product>(`${this.api}/${id}`, product, httpOptions);
+    return this.http.put<Product>(`${this.api}/${id}`, product, {
+      headers: this.httpOptions,
+    });
   }
 
   deleteProduct(id: string): Observable<Product> {
-    return this.http.delete<Product>(`${this.api}/${id}`);
+    return this.http.delete<Product>(`${this.api}/${id}`, {
+      headers: this.httpOptions,
+    });
   }
 }

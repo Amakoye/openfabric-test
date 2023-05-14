abd204fdf39ac01676a7f2c3f51d5efbd6bc5bbc
Amakoye <charlesamakoye@gmail.com>
2023-05-14 21:43:15 +0300

    add env variables for backend

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 
+dotenv.config();
+
 const connectDB = async () => {
+  console.log(process.env.MONGODB_URI);
   try {
-    const connectionUri =
-      "mongodb+srv://charlesamakoye:FbbZwX9NLBF4FyLP@cluster2023.inetwf2.mongodb.net/open_fabric_db?retryWrites=true&w=majority";
+    const connectionUri = `${process.env.MONGODB_URI}`;
     await mongoose.connect(connectionUri);
 
     const db = mongoose.connection;
